3adb89786b13cde1b1e30a6db58e4eaa81e4647b
Melih Şişkular <feis_melih@hotmail.com>
2026-08-25 07:04:37 +0300

    Initial Commit

---
+++ ItalianVocabulary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ItalianVocabulary
+//
+//  Created by Melih Şişkular on 25.08.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ItalianVocabulary/ItalianVocabularyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItalianVocabularyApp.swift
+//  ItalianVocabulary
+//
+//  Created by Melih Şişkular on 25.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct ItalianVocabularyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
